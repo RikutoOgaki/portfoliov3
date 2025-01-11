b4f1ade301c847587b6d25f3f1600bebe96de7d6
Rikuto <orikutobasuke@gmail.com>
2025-01-12 04:34:02 +0900

    create header styles

--- src/components/template/Header.tsx
+++ src/components/template/Header.tsx
@@ -1,10 +1,19 @@
 'use client'
-import { useState, useEffect } from "react";
+
+import { useState, useEffect, useRef } from 'react'
+import styles from '@/styles/components/template/header.module.css'
 
 export default function Header() {
+
+
   return (
-    <>
-      <header></header>
-    </>
+    <header className={styles.headerWrap}>
+      <ul className={styles.listBox}>
+        <li><a href="">TOP</a></li>
+        <li><a href="">ABOUT</a></li>
+        <li><a href="">SERVICE</a></li>
+        <li><a href="">CONTACT</a></li>
+      </ul>
+    </header>
   )
 }
