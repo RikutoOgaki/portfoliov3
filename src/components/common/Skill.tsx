532f1e8dcc444e63aee587bea92a23a149d16f5c
Rikuto <orikutobasuke@gmail.com>
2025-01-21 17:56:17 +0900

    fix: create skill Component

---
+++ src/components/common/Skill.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+
+export default function Skill() {
+    return (
+        <>
+            <div></div>
+        </>
+    )
+}
---
+++ src/data/skillData.ts
@@ -0,0 +1,4 @@
+import { FaHtml5, FaCss3Alt, FaReact, FaSass } from 'react-icons/fa';
+import { SiJavascript, SiTypescript, SiNextdotjs } from "react-icons/si";
+
+// export const skillData = 
